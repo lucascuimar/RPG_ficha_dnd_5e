817d148035eef383add7e93648a1b86683358cde
maconha <lucascuimar23@gmail.com>
2019-09-09 16:50:23 -0300

    organisação leia mais

--- resources/views/informacao/leia_mais.blade.php
+++ resources/views/informacao/leia_mais.blade.php
@@ -9,18 +9,59 @@ Temos um pouco sobre nós.
 @endsection
 
 @section('corpo')
-<p>Lorem ipsum senectus euismod ligula class, rhoncus gravida molestie consectetur, cubilia viverra pulvinar magna. blandit per tincidunt luctus mollis lacus morbi placerat, purus amet egestas elementum quam ante habitant et, aptent taciti ligula eget donec platea. dictumst dapibus nulla risus proin risus id integer sodales, duis litora taciti nisi senectus molestie per proin nunc, odio cursus habitant sollicitudin cras sapien aptent. torquent auctor taciti sapien molestie aliquet proin ante turpis aliquam, proin enim molestie nibh tempus lorem per porta quisque convallis, phasellus erat ultrices rutrum ornare vel lectus litora. vehicula hac sociosqu habitant curabitur accumsan dictumst praesent dui torquent etiam, semper bibendum proin nisl ut mauris potenti convallis.</p>
+<p>Lorem ipsum senectus euismod ligula class, rhoncus gravida molestie
+  consectetur, cubilia viverra pulvinar magna. blandit per tincidunt luctus
+  mollis lacus morbi placerat, purus amet egestas elementum quam ante habitant
+  et, aptent taciti ligula eget donec platea. dictumst dapibus nulla risus proin
+  risus id integer sodales, duis litora taciti nisi senectus molestie per proin nunc,
+  odio cursus habitant sollicitudin cras sapien aptent. torquent auctor taciti
+  sapien molestie aliquet proin ante turpis aliquam, proin enim molestie nibh
+  tempus lorem per porta quisque convallis, phasellus erat ultrices rutrum ornare vel
+  lectus litora. vehicula hac sociosqu habitant curabitur accumsan dictumst praesent
+  dui torquent etiam, semper bibendum proin nisl ut mauris potenti convallis.</p>
 
-<p>Mattis porttitor nulla fringilla a adipiscing varius, etiam ante etiam fames senectus, tortor aenean posuere lectus potenti. tempor vivamus imperdiet justo erat habitasse nisi tortor tempor risus volutpat fermentum, hendrerit suscipit faucibus tempus congue morbi commodo lorem inceptos urna non aliquet, aliquam cras fringilla curabitur vivamus ut gravida luctus vehicula fusce. gravida aliquam donec lacus et porta dictumst imperdiet tincidunt fringilla faucibus orci, blandit litora eget ornare molestie praesent lectus augue eu. aenean etiam eu vivamus euismod donec velit proin habitasse, hendrerit curabitur ad dapibus curabitur luctus convallis sem, pulvinar hac leo sit nec ut felis.</p>
+<p>Mattis porttitor nulla fringilla a adipiscing varius, etiam ante etiam fames
+  senectus, tortor aenean posuere lectus potenti. tempor vivamus imperdiet justo
+  erat habitasse nisi tortor tempor risus volutpat fermentum, hendrerit suscipit
+  faucibus tempus congue morbi commodo lorem inceptos urna non aliquet, aliquam
+  cras fringilla curabitur vivamus ut gravida luctus vehicula fusce. gravida
+  aliquam donec lacus et porta dictumst imperdiet tincidunt fringilla faucibus
+  orci, blandit litora eget ornare molestie praesent lectus augue eu. aenean
+  etiam eu vivamus euismod donec velit proin habitasse, hendrerit curabitur ad
+  dapibus curabitur luctus convallis sem, pulvinar hac leo sit nec ut felis.</p>
 
-<p>Fermentum fames convallis vitae sollicitudin platea magna cras ligula nam tortor, aliquam ut curabitur rutrum posuere enim nulla ultricies fusce. quam varius augue eros quisque euismod diam pharetra, convallis odio donec luctus aliquam lobortis velit, turpis tempus ultricies dapibus bibendum vestibulum. facilisis turpis faucibus elit et fusce lobortis quam quisque suscipit, habitant est eros torquent ante porta mauris. vehicula enim feugiat a mi torquent ante varius cubilia, nisi donec curabitur rhoncus pretium cubilia ut, quisque cubilia laoreet lorem urna elementum enim. quis quisque eu phasellus nam augue tempus suscipit massa, nostra erat ante magna metus ac consequat tortor ipsum, ligula lacinia pharetra in ante vehicula luctus.
+<p>Fermentum fames convallis vitae sollicitudin platea magna cras ligula nam
+  tortor, aliquam ut curabitur rutrum posuere enim nulla ultricies fusce. quam
+  varius augue eros quisque euismod diam pharetra, convallis odio donec luctus
+  aliquam lobortis velit, turpis tempus ultricies dapibus bibendum vestibulum.
+  facilisis turpis faucibus elit et fusce lobortis quam quisque suscipit,
+  habitant est eros torquent ante porta mauris. vehicula enim feugiat a mi
+  torquent ante varius cubilia, nisi donec curabitur rhoncus pretium cubilia ut,
+  quisque cubilia laoreet lorem urna elementum enim. quis quisque eu phasellus
+  nam augue tempus suscipit massa, nostra erat ante magna metus ac consequat
+  tortor ipsum, ligula lacinia pharetra in ante vehicula luctus.
 
-<p>Ante lobortis convallis a vestibulum odio dapibus, primis convallis quis integer vulputate aptent, mi ut felis eleifend litora. mi habitasse sed nisi fusce fames mi maecenas himenaeos dictum quisque aenean nisi varius magna orci, iaculis eleifend litora purus lectus dui donec felis per pulvinar luctus vulputate quam. curabitur fringilla habitant odio maecenas curae pretium varius suscipit pellentesque, imperdiet habitasse scelerisque ultrices vehicula arcu per praesent, feugiat posuere pharetra orci vulputate feugiat a curabitur. nec dui tempus class commodo sed et curabitur pretium ornare convallis dictum ligula hendrerit aliquam praesent, dapibus phasellus orci eros mi in mi in nostra pretium bibendum nullam vivamus.</p>
+<p>Ante lobortis convallis a vestibulum odio dapibus, primis convallis quis integer
+  vulputate aptent, mi ut felis eleifend litora. mi habitasse sed nisi fusce
+  fames mi maecenas himenaeos dictum quisque aenean nisi varius magna orci,
+  iaculis eleifend litora purus lectus dui donec felis per pulvinar luctus
+  vulputate quam. curabitur fringilla habitant odio maecenas curae pretium varius
+  suscipit pellentesque, imperdiet habitasse scelerisque ultrices vehicula arcu
+  per praesent, feugiat posuere pharetra orci vulputate feugiat a curabitur. nec
+  dui tempus class commodo sed et curabitur pretium ornare convallis dictum
+  ligula hendrerit aliquam praesent, dapibus phasellus orci eros mi in mi in
+  nostra pretium bibendum nullam vivamus.</p>
 
-<p>Aenean sagittis litora tempor quam condimentum mi posuere, dapibus auctor suspendisse ac magna erat luctus, ultricies elementum hendrerit facilisis elit porta. integer rutrum aliquam euismod nisl vulputate, malesuada nibh vitae senectus odio, elementum scelerisque nibh odio. mi class dui mauris ullamcorper etiam quam suspendisse a hendrerit cras ad etiam, pulvinar dui auctor sit nibh nullam aptent habitant purus quis. iaculis elit feugiat imperdiet hac suscipit urna hac orci velit volutpat, urna lacus ipsum congue etiam lacus venenatis platea sem, purus hendrerit velit pharetra fames tempus tempor facilisis per.</p>
+<p>Aenean sagittis litora tempor quam condimentum mi posuere, dapibus auctor
+  suspendisse ac magna erat luctus, ultricies elementum hendrerit facilisis elit
+  porta. integer rutrum aliquam euismod nisl vulputate, malesuada nibh vitae
+  senectus odio, elementum scelerisque nibh odio. mi class dui mauris ullamcorper
+  etiam quam suspendisse a hendrerit cras ad etiam, pulvinar dui auctor sit nibh
+  nullam aptent habitant purus quis. iaculis elit feugiat imperdiet hac suscipit
+  urna hac orci velit volutpat, urna lacus ipsum congue etiam lacus venenatis
+  platea sem, purus hendrerit velit pharetra fames tempus tempor facilisis per.</p>
 @endsection
 
 @section('navegacao')
-<b>
 <a href="/" class="btn btn-dark mb-2">Menu</a>
 @endsection
